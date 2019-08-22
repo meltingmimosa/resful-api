d9144572f50685f11996d4db7f85cd2bacb29d90
unknown <meltingmimosa@gmail.om>
2019-08-22 14:57:13 +0800

    workshop03 setup

--- workshop03/main.js
+++ workshop03/main.js
@@ -41,6 +41,123 @@ app.use(express.urlencoded({ extended: true }));
 
 // Start of workshop
 // TODO 2/2 Copy your routes from workshop02 here
+// Mandatory workshop
+// TODO GET /api/states
+// TODO GET /api/states
+
+app.get(
+	'/api/states',
+	(req,resp) => {
+		db.findAllStates()
+		.then(result => {
+			resp.status(200);
+			resp.type('application/json');
+			resp.json(//result
+				result.map(v =>`/api/state/${v}`));
+		}		
+		)
+		.catch(error =>
+		{
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		}
+		)
+	}
+)
+
+//TODO HEAD /api/state/:state
+app.head(
+	'/api/state/:state',
+	(req,resp) => {
+		//should check if state exist
+		resp.status(200);
+		resp.type("application/json");
+		resp.header('Accept-Ranges','items');
+		resp.end(); //test
+	}
+)
+
+
+// TODO GET /api/state/:state
+app.get(
+	'/api/state/:state',
+	range({accept:'cities',limit:20}),
+	(req,resp) => {
+		const state = req.params.state;
+		const first = req.range.first;
+		const last = req.range.last;
+
+		//count cities in state
+		//results containing 2 arrays
+		Promise.all([
+			db.findCitiesByState(state,{limit: (last-first +1), offset:first}),
+			db.countCitiesInState(state)
+		])
+			.then(results => {
+				//resp.status(200);
+				resp.status(206);
+				resp.type('application/json');
+				resp.range({
+					first:first,
+					last:last,
+					length:results[1]
+				})
+				resp.json(results[0].map(v => `/api/city/${v}`));
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
+)
+
+
+
+// TODO GET /api/city/:cityId
+app.get(
+	'/api/city/:cityId',
+	(req,resp) => {
+		const cityId = req.params.cityId;
+		db.findCityById(cityId)
+			.then(result => {
+				resp.status(200);
+				resp.type('application/json');
+				resp.json(result);
+			})
+			.catch(error => {
+				resp.status(400);
+				resp.type('text/plain');
+				resp.send(error);
+			})
+	}
+)
+
+
+
+// TODO POST /api/city
+//application/x-www-form-urlencided - body
+app.post(
+	'/api/city',
+	(req,resp) => {
+		const data = req.body
+		console.info('>> data:', data)
+
+		db.insertCity(data)
+		.then (result => {
+			resp.status(201)
+			resp.type('application/json')
+			resp.json({message:'added'});
+		})
+		.catch(error => {
+			resp.status(400);
+			resp.type('text/plain');
+			resp.send(error);
+		})
+
+	}
+)
 
 
 // End of workshop
